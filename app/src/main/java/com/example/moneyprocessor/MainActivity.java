8e72109fa2e590ceed731611c7b26a5d6a612864
Your Name <you@example.com>
2021-11-23 23:10:28 -0300

    adding redux alerts

--- app/src/main/java/com/example/moneyprocessor/MainActivity.java
+++ app/src/main/java/com/example/moneyprocessor/MainActivity.java
@@ -48,17 +48,20 @@ public class MainActivity extends IntroActivity {
         );
 
         addSlide(new FragmentSlide.Builder()
-                .background(android.R.color.holo_purple)
+                .background(android.R.color.holo_green_light)
                 .fragment(R.layout.intro_3)
                 .build()
         );
 
 
         addSlide(new FragmentSlide.Builder()
-                .background(android.R.color.holo_red_light)
+                .background(android.R.color.holo_purple)
                 .fragment(R.layout.intro_4)
+                // nao fechar apos ultimo slide
+                .canGoForward(false)
                 .build()
         );
+
 
 
     }
